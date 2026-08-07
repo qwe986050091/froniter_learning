d342b97f408e806124ce83f73f4a8a9c5846bb2a
zhanglingfeng <2062914397@qq.com>
2026-08-08 02:06:27 +0800

    增加环境变量

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,5 +1,15 @@
 /// <reference types="vite/client" />
 
+interface ImportMetaEnv {
+  readonly VITE_APP_ENV: 'development' | 'ppe' | 'production'
+  readonly VITE_APP_TITLE: string
+  readonly VITE_API_BASE_URL: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
+
 declare module '*.vue' {
   import type { DefineComponent } from 'vue'
   const component: DefineComponent<{}, {}, any>
